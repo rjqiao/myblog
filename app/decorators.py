8d1cffc67134afc2eb6531aaa9f79db74971e0ed
rjqiao <rjqiao@gmail.com>
2017-12-27 17:26:11 -0800

    no message

--- app/decorators.py
+++ app/decorators.py
@@ -2,10 +2,12 @@ from functools import wraps
 from flask import abort
 from flask_login import current_user
 
+
 def admin_required(func):
     @wraps(func)
     def wrapper(*args, **kw):
         if not current_user.is_administrator():
-			abort(403)
-		return func(*args, **kw)
-	return wrapper
+            abort(403)
+        return func(*args, **kw)
+
+    return wrapper
